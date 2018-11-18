9072808d05a198bf82a20f59276c027b02221e72
Petr Chebanenko <Petr_Chebanenko@epam.com>
2018-11-18 17:10:28 +0600

    Changes with  date(some refactoring)

--- pages/scripts/tasks-script.js
+++ pages/scripts/tasks-script.js
@@ -35,7 +35,23 @@ function fillTaskListUl(tasksData) {
     for (let index = 0; index < tasksData.length; index++) {
         var listItem = document.createElement("li");
 
+        var isDoneCheckBox = document.createElement("input");
+        isDoneCheckBox.setAttribute("type", "checkbox");
+        //isDoneCheckBox.setAttribute('defaultValue', false);
+        isDoneCheckBox.setAttribute('id', tasksData[index].id);
+
+        if(tasksData[index].isDone === true)
+        {
+            isDoneCheckBox.setAttribute('disabled', true);
+            isDoneCheckBox.setAttribute('checked', true);
+        }
+        else
+        {
+            isDoneCheckBox.setAttribute('checked', false);
+        }
+
         listItem.appendChild(document.createTextNode(tasksData[index].taskName + " " + tasksData[index].taskDeadline));
+        listItem.appendChild(isDoneCheckBox);
         taskList.appendChild(listItem);        
     }
 }
@@ -46,70 +62,94 @@ var otherDayButton = document.getElementById("otherDayButton");
 var deadlineLable = document.getElementById("deadlineLable");
 var deadlineDate;
 
-function deadLineButtonsClick() {
+/*function deadLineButtonsClick() {
     todayDealineButton.disabled = true;
     yesterdayDeadlineButton.disabled = true;
     otherDayButton.disabled = true;
-}
+}*/
 
-todayDealineButton.addEventListener("click", function() {
-    function getTodaysDate() {
-        var date = new Date();
-        var day = date.getDate();
-        var month = date.getMonth() + 1;
-        var year = date.getFullYear();
-
-        if(day < 10){
-            day = "0" + day;
-        }
-
-        if(month < 10) {
-            month = "0" + month;
-        }
-
-        date = day + "/" + month + "/" + year;
-        return date;
-    }
-
-    deadlineDate = getTodaysDate();
+todayDealineButton.addEventListener("click", function() {   
+    deadlineDate = formatDate(null);
     deadlineLable.innerText = deadlineDate;
-    deadLineButtonsClick();
 });
 
 yesterdayDeadlineButton.addEventListener("click", function() {
-    function getYesterDaysDate()
-    {
-        var date = new Date();
-        var day = date.getDate() - 1;
-        var month = date.getMonth() + 1;
-        var year = date.getFullYear();
+    var todayDate = new Date();
+    var yesterdayDate = new Date(todayDate);
+    yesterdayDate.setDate(todayDate.getDate() - 1);
 
-        if(day < 10){
-            day = "0" + day;
-        }
-
-        if(month < 10) {
-            month = "0" + month;
-        }
-
-        date = day + "/" + month + "/" + year;
-        return date;
-    }
-    deadlineDate = getYesterDaysDate();
+    deadlineDate = formatDate(yesterdayDate);
     deadlineLable.innerText = deadlineDate;
-    deadLineButtonsClick();
 });
 
-function createTask() {
-    var taskNameInput =  document.getElementById("taskName");
-    var newTask = ({
-        taskName: taskNameInput.value,
-        taskDeadline: deadlineDate,
-        isDone: false
-    });
+otherDayButton.addEventListener("click", function()
+{
+    var modalWindow = document.getElementById("otherDayModal");
+    var closeSpan = document.getElementsByClassName("close")[0];
 
-    createNewTask(newTask);
-    location.reload(true);//true - reloads from server; false(default) - reloads from the cache
+    modalWindow.style.display = "block";
+    
+    closeSpan.onclick = function(){
+        var calendar = document.getElementsByClassName("other-day")[0];
+        if(calendar) {
+            deadlineDate = formatDate(calendar.value);
+            deadlineLable.innerText = deadlineDate;
+        }
+
+        modalWindow.style.display = "none";
+    }
+})
+
+function validateAdding(element){
+    if(!element.checkValidity()){
+        document.getElementById("taskError").innerHTML = element.validationMessage;
+        return false;
+    }
+
+    return true;
+}
+
+
+function createTask() {
+    var taskNameInput = document.getElementById("taskName");
+
+    var isValid = validateAdding(taskNameInput);
+
+    if(isValid) {
+        var newTask = ({
+            taskName: taskNameInput.value,
+            taskDeadline: deadlineDate,
+            isDone: false
+        });
+
+        createNewTask(newTask, validateAdding(taskNameInput));
+        location.reload(true);//true - reloads from server; false(default) - reloads from the cache
+    }
 };
 
 createTaskButton.addEventListener("click", createTask);
+
+function formatDate(date) {
+    var taskDate;
+
+    if(date) {
+        taskDate = new Date(date);
+    }
+    else {
+        taskDate = new Date();
+    }
+    var day = taskDate.getDate();
+    var month = taskDate.getMonth() + 1;
+    var year = taskDate.getFullYear();
+
+    if(day < 10){
+        day = "0" + day;
+    }
+
+    if(month < 10) {
+        month = "0" + month;
+    }
+
+    date = day + "/" + month + "/" + year;
+    return date;
+}
